28082c9509d1d4c3e2a747555b05e2960e3a9f71
Pratyush Garg <pratyushhg@gmail.com>
2021-04-26 11:32:30 +0530

    endpoints createx

--- src/server.js
+++ src/server.js
@@ -10,6 +10,27 @@ app.disable('x-powered-by')
 app.use(cors())
 app.use(json())
 app.use(urlencoded({ extended: true }))
-app.use(morgan('dev'))
+app.use(morgan('dev')) // logger
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'OK' })
+})
+
+app.get('/data', (req, res) => {
+  res.send({ message: 'Hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log('server is on 3000')
+  })
+}
