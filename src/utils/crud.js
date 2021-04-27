d1603f1529a3c34d5720e83e61736461a3342b15
Pratyush Garg <pratyushhg@gmail.com>
2021-04-27 16:35:29 +0530

    error code corrected

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -6,13 +6,13 @@ export const getOne = model => async (req, res) => {
       .exec()
 
     if (!doc) {
-      return res.status(400).end()
+      return res.status(404).end()
     }
 
     res.status(200).json({ data: doc })
   } catch (e) {
     console.error(e)
-    res.status(400).end()
+    res.status(404).end()
   }
 }
 
