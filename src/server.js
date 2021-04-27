f2e5e89f843419ad6b0077fbf31a7351d01a1195
Pratyush Garg <pratyushhg@gmail.com>
2021-04-27 16:32:02 +0530

    authorization added to routes

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import { connect } from './utils/db'
 import userRouter from './resources/user/user.router'
 import itemRouter from './resources/item/item.router'
 import listRouter from './resources/list/list.router'
+import { protect, signin, signup } from './utils/auth'
 
 export const app = express()
 const router = express.Router()
@@ -73,6 +74,9 @@ app.post('/data', (req, res) => {
 })
 
 app.use(morgan('dev')) // logger
+app.use('/signup', signup)
+app.use('/signin', signin)
+app.use('/api', protect)
 app.use('/api/user', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/list', listRouter)
